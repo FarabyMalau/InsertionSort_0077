8e1c710276136f62df9a0b225ec4f90b4392a137
FarabyMalau <farabymuzakki49@gmail.com>
2025-03-26 02:20:18 +0600

    Menginput nilai data n dan menyimpan nilai data n kedalam array arr

--- InsertionSort.cpp
+++ InsertionSort.cpp
@@ -26,5 +26,8 @@ void input()
     cout << "======================" << endl;
 
 for (int i = 0; i < n; i++) // Menggunakan perulangan for untuk menyimpan data pada array
-{}
+{
+    cout << "Data ke-" << (i + 1) << ": "; // Memasukkan atau menginputkan nilai data n
+    cin >> arr[i]; // Menyimpan nilai data n kedalam array arr
+}
 }
